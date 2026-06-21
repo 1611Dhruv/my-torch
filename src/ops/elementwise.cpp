a167fb8cef464ac59b1868282925a528d2aabf15
Dhruv Desai <iamdhruv1611@gmail.com>
2026-06-20 19:58:51 -0500

    Simplified add_kernel into just unary and binary

--- src/ops/elementwise.cpp
+++ src/ops/elementwise.cpp
@@ -4,8 +4,57 @@
 namespace torch {
 namespace cpu {
 
-template <typename T> static void add_kernel(const Tensor &a, const Tensor &b, Tensor &out) {
+// Helper which will walk through one tensor applying op and saving in out
+template <typename T, typename Op> static void unary_elementwise(const Tensor &a, Tensor &out, Op op) {
+  if (!out.is_contiguous() || out.shape() != a.shape()) {
+    throw std::runtime_error("out wasn't contiguous or had a different shape");
+  }
 
+  const T *a_data = a.data_ptr<T>();
+  T *o_data = out.data_ptr<T>();
+
+  // if both are contiguous that means its a simple op
+  // Go through the entire frame and just add
+  if (a.is_contiguous()) {
+    auto N = a.numel();
+    for (int64_t i = 0; i < N; i++) {
+      o_data[i] = op(a_data[i]);
+    }
+    return;
+  }
+
+  const auto shape = a.shape();
+  int64_t N_DIM = shape.size();
+  struct Frame {
+    int64_t dim;
+    int64_t a_off;
+    int64_t o_off;
+  };
+
+  std::stack<Frame> frames;
+  frames.push({0, 0, 0, 0});
+
+  while (!frames.empty()) {
+    auto f = frames.top();
+    frames.pop();
+    if (f.dim == N_DIM) {
+      o_data[f.o_off] = op(a_data[f.a_off]);
+    } else {
+      // We go add this dims kids
+      for (int i = 0; i < shape[f.dim]; i++) {
+        frames.push({
+            f.dim + 1,
+            f.a_off + i * a.strides()[f.dim],
+            f.o_off + i * out.strides()[f.dim],
+        });
+      }
+    }
+  }
+}
+
+// Helper which will walk through the two tensors applying op and saving  in out
+template <typename T, typename Op>
+static void binary_elementwise(const Tensor &a, const Tensor &b, Tensor &out, Op op) {
   if (!out.is_contiguous() || out.shape() != a.shape()) {
     throw std::runtime_error("out wasn't contiguous or had a different shape");
   }
@@ -19,12 +68,11 @@ template <typename T> static void add_kernel(const Tensor &a, const Tensor &b, T
   if (a.is_contiguous() && b.is_contiguous()) {
     auto N = a.numel();
     for (int64_t i = 0; i < N; i++) {
-      o_data[i] = a_data[i] + b_data[i];
+      o_data[i] = op(a_data[i], b_data[i]);
     }
     return;
   }
 
-  // Otherwise we need to recurse and explore dim by dim
   const auto shape = a.shape();
   int64_t N_DIM = shape.size();
   struct Frame {
@@ -41,7 +89,7 @@ template <typename T> static void add_kernel(const Tensor &a, const Tensor &b, T
     auto f = frames.top();
     frames.pop();
     if (f.dim == N_DIM) {
-      o_data[f.o_off] = a_data[f.a_off] + b_data[f.b_off];
+      o_data[f.o_off] = op(a_data[f.a_off], b_data[f.b_off]);
     } else {
       // We go add this dims kids
       for (int i = 0; i < shape[f.dim]; i++) {
@@ -69,7 +117,8 @@ Tensor add(const Tensor &a, const Tensor &b) {
   }
 
   Tensor out = torch::Tensor::zeros(a.shape(), a.dtype(), a.device());
-  DISPATCH_OP(a.dtype(), [&] { add_kernel<scalar_t>(a, b, out); });
+  DISPATCH_OP(a.dtype(),
+              [&] { binary_elementwise<scalar_t>(a, b, out, [](scalar_t x, scalar_t y) { return x + y; }); });
   return out;
 }
 
