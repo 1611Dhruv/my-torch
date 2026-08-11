f0ba0642aa39cdbb084b6fef519511172170f907
Dhruv Desai <iamdhruv1611@gmail.com>
2026-08-11 16:36:47 -0500

    Updated matmul

--- src/ops/matmul.cu
+++ src/ops/matmul.cu
@@ -7,51 +7,164 @@
 namespace torch {
 namespace cuda {
 
-template <typename scalar_t, const int TILE_WIDTH>
-__global__ void matmul_kernel(const scalar_t *a, const scalar_t *b, scalar_t *out, int64_t n1, int64_t n2, int64_t n3) {
-  int64_t bx = blockIdx.x, by = blockIdx.y;
-  int64_t tx = threadIdx.x, ty = threadIdx.y;
+/*
+ * Double buffered, warp tiled, reg tiled Matmul
+ *
+ * Reduces a M * K and K * N matrix down to M * N
+ * Accounts for original ones being transposed or not
+ */
 
-  int64_t i = TILE_WIDTH * by + ty;
-  int64_t j = TILE_WIDTH * bx + tx;
+template <typename scalar_t, const bool a_transp, const bool b_transp, const int BM = 128, const int BN = 128,
+          const int BK = 4, const int WM = 64, const int WN = 64, const int TM = 8, const int TN = 4,
+          const int WN_ITER = 2>
+__global__ void matmul_kernel(const scalar_t *__restrict__ A, const scalar_t *__restrict__ B, scalar_t *__restrict__ C,
+                              int M, int K, int N) {
+  // +1 padding for non coalasced things
+  __shared__ scalar_t As[2][BK][BM + 1];
+  __shared__ scalar_t Bs[2][BK][BN + 1];
 
-  __shared__ scalar_t sh_A[TILE_WIDTH][TILE_WIDTH];
-  __shared__ scalar_t sh_B[TILE_WIDTH][TILE_WIDTH];
+  // Some asserts for this reg + warp tiled + double buffered kernel
+  constexpr int WM_ITER = WM * WN / (32 * TM * TN) / WN_ITER;
+  constexpr int LANE_ROWS = WM / WM_ITER / TM;
+  constexpr int LANE_COLS = WN / WN_ITER / TN;
+  constexpr int WM_STMP = LANE_ROWS * TM;
+  constexpr int WN_STMP = LANE_COLS * TN;
 
-  scalar_t partial_dot = 0;
-  int64_t tiles = (n2 + TILE_WIDTH - 1) / TILE_WIDTH;
-  for (int64_t k = 0; k < tiles; k++) {
-    if ((i < n1) && (k * TILE_WIDTH + tx < n2)) {
-      sh_A[ty][tx] = a[i * n2 + k * TILE_WIDTH + tx];
+  static_assert(WM_ITER * WN_ITER * TM * TN * 32 == WM * WN, "The warp tiled configuration do not match");
+  static_assert(LANE_ROWS * LANE_COLS == 32, "The warp must have only 32 lanes");
+
+  int roff = blockIdx.y * BM;
+  int coff = blockIdx.x * BN;
+  int tid = threadIdx.x;
+  int wid = tid >> 5;
+  int wroff = (wid / (BN / WN)) * WM;
+  int wcoff = (wid % (BN / WN)) * WN;
+  int lane = tid & 31;
+
+  int tt = (BM * BN / WM / WN * 32);
+
+  int pw = 0;
+  int pr = 1;
+
+  auto load_a = [&](int koff) {
+    if constexpr (a_transp) {
+      for (int i = tid; i < BK * BM; i += tt) {
+        int lr = i / BM;
+        int lc = i % BM;
+
+        int gr = koff + lr;
+        int gc = roff + lc;
+        As[pw][lr][lc] = (gr < K && gc < M) ? A[gr * M + gc] : scalar_t{};
+      }
     } else {
-      sh_A[ty][tx] = 0;
-    }
+      for (int i = tid; i < BK * BM; i += tt) {
+        int lr = i / BK;
+        int lc = i % BK;
 
-    if ((j < n3) && (k * TILE_WIDTH + ty < n2)) {
-      sh_B[ty][tx] = b[(k * TILE_WIDTH + ty) * n3 + j];
+        int gr = roff + lr;
+        int gc = koff + lc;
+        As[pw][lc][lr] = (gr < M && gc < K) ? A[gr * K + gc] : scalar_t{};
+      }
+    }
+  };
+
+  auto load_b = [&](int koff) {
+    if constexpr (b_transp) {
+      for (int i = tid; i < BK * BN; i += tt) {
+        int lr = i / BK;
+        int lc = i % BK;
+
+        int gr = coff + lr;
+        int gc = koff + lc;
+        Bs[pw][lc][lr] = (gr < N && gc < K) ? B[gr * K + gc] : scalar_t{};
+      }
     } else {
-      sh_B[ty][tx] = 0;
-    }
-    __syncthreads();
+      for (int i = tid; i < BK * BN; i += tt) {
+        int lr = i / BN;
+        int lc = i % BN;
 
-    for (int l = 0; l < TILE_WIDTH; l++) {
-      partial_dot += sh_A[ty][l] * sh_B[l][tx];
+        int gr = koff + lr;
+        int gc = coff + lc;
+        Bs[pw][lr][lc] = (gr < K && gc < N) ? B[gr * N + gc] : scalar_t{};
+      }
     }
+  };
+
+  // Thread registers
+  scalar_t a_reg[WM_ITER][TM];
+  scalar_t b_reg[WN_ITER][TN];
+  scalar_t acc[WM_ITER][WN_ITER][TM][TN] = {};
+
+  auto load_a_reg = [&](int k, int wi) {
+    int warp_off = wi * WM_STMP;
+    for (int i = 0; i < TM; i++)
+      a_reg[wi][i] = As[pr][k][wroff + warp_off + (lane / LANE_COLS) * TM + i];
+  };
+
+  auto load_b_reg = [&](int k, int wj) {
+    int warp_off = wj * WN_STMP;
+    for (int j = 0; j < TN; j++)
+      b_reg[wj][j] = Bs[pr][k][wcoff + warp_off + (lane % LANE_COLS) * TN + j];
+  };
+
+  // Actual Matmul
+  load_a(0);
+  load_b(0);
+  __syncthreads();
+  for (int koff = 0; koff < K; koff += BK) {
+    pw ^= 1;
+    pr ^= 1;
+    if (koff + BK < K) {
+      load_a(koff + BK);
+      load_b(koff + BK);
+    }
+
+    for (int k = 0; k < BK; k++) {
+      // Load the warps into regs
+      for (int wi = 0; wi < WM_ITER; wi++) {
+        load_a_reg(k, wi);
+      }
+
+      for (int wj = 0; wj < WN_ITER; wj++) {
+        load_b_reg(k, wj);
+      }
+
+      // Perform the FMA
+      for (int wi = 0; wi < WM_ITER; wi++) {
+        for (int wj = 0; wj < WN_ITER; wj++) {
+          for (int i = 0; i < TM; i++) {
+            for (int j = 0; j < TN; j++) {
+              acc[wi][wj][i][j] += a_reg[wi][i] * b_reg[wj][j];
+            }
+          }
+        }
+      }
+    }
+
     __syncthreads();
   }
 
-  if ((i < n1) && (j < n3))
-    out[i * n3 + j] = partial_dot;
+  for (int wi = 0; wi < WM_ITER; wi++) {
+    for (int wj = 0; wj < WN_ITER; wj++) {
+      for (int i = 0; i < TM; i++) {
+        for (int j = 0; j < TN; j++) {
+          int r = roff + wroff + wi * WM_STMP + (lane / LANE_COLS) * TM + i;
+          int c = coff + wcoff + wj * WN_STMP + (lane % LANE_COLS) * TN + j;
+
+          if (r < M && c < N) {
+            C[r * N + c] = acc[wi][wj][i][j];
+          }
+        }
+      }
+    }
+  }
 }
 
 Tensor matmul(const Tensor &a, const Tensor &b) {
   assert(a.dtype() == b.dtype());
   assert(a.shape().size() == 2 && b.shape().size() == 2);
   assert(a.shape()[1] == b.shape()[0]);
-
-  if (!a.is_contiguous() || !b.is_contiguous()) {
-    throw std::invalid_argument("cuda matmul: tensors must be contiguous"); // Make contiguous() cuda aware
-  }
+  assert(a.device() == Device::CUDA);
 
   int64_t n1 = a.shape()[0];
   int64_t n2 = a.shape()[1];
@@ -59,17 +172,30 @@ Tensor matmul(const Tensor &a, const Tensor &b) {
 
   Tensor out({n1, n3}, a.dtype(), a.device());
 
-  constexpr int TILE_WIDTH = 32;
-  dim3 grid((n3 + TILE_WIDTH - 1) / TILE_WIDTH, (n1 + TILE_WIDTH - 1) / TILE_WIDTH);
-  dim3 block(TILE_WIDTH, TILE_WIDTH);
+  dim3 grid((n3 + 127) / 128, (n1 + 127) / 128);
+  dim3 block(128);
+
+  bool at = !a.is_contiguous();
+  bool bt = !b.is_contiguous();
 
   DISPATCH_OP(a.dtype(), [&] {
-    matmul_kernel<scalar_t, TILE_WIDTH>
-        <<<grid, block>>>(a.data_ptr<scalar_t>(), b.data_ptr<scalar_t>(), out.data_ptr<scalar_t>(), n1, n2, n3);
+    if (at && bt) {
+      matmul_kernel<scalar_t, true, true>
+          <<<grid, block>>>(a.data_ptr<scalar_t>(), b.data_ptr<scalar_t>(), out.data_ptr<scalar_t>(), n1, n2, n3);
+    } else if (at && !bt) {
+      matmul_kernel<scalar_t, true, false>
+          <<<grid, block>>>(a.data_ptr<scalar_t>(), b.data_ptr<scalar_t>(), out.data_ptr<scalar_t>(), n1, n2, n3);
+    } else if (!at && bt) {
+      matmul_kernel<scalar_t, false, true>
+          <<<grid, block>>>(a.data_ptr<scalar_t>(), b.data_ptr<scalar_t>(), out.data_ptr<scalar_t>(), n1, n2, n3);
+    } else {
+      matmul_kernel<scalar_t, false, false>
+          <<<grid, block>>>(a.data_ptr<scalar_t>(), b.data_ptr<scalar_t>(), out.data_ptr<scalar_t>(), n1, n2, n3);
+    }
   });
 
   CUDA_CHECK(cudaGetLastError());
-  CUDA_CHECK(cudaDeviceSynchronize()); // TODO: Remove in prod
+  CUDA_CHECK(cudaDeviceSynchronize());
 
   return out;
 }
